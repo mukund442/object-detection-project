eae8c3bca9097620419bb9c49e2569d0ab99c7d6
mukund442 <92722963+mukund442@users.noreply.github.com>
2022-10-12 16:47:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function setup(){
     canvas=createCanvas(640, 420);
     canvas.center();
+
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("ststus").innerHTML="Status : detecting objects";
 }
 
 img="";
@@ -15,4 +18,24 @@ function draw(){
     noFill();
     stroke("#00FFFF");
     rect(30, 60, 450, 350);
+
+    fill("#00FFFF");
+    text("apples", 45, 75);
+    noFill();
+    stroke("#00FFFF");
+    rect(30, 60, 450, 350);
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
 }
